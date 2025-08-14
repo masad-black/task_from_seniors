12ba2fd40f28577280376fcd7f23f50ca351e3fa
masad@black <asadbutt.dev@gmail.com>
2025-08-15 00:01:02 +0500

    docker

--- phones_api/src/index.js
+++ phones_api/src/index.js
@@ -5,7 +5,7 @@ const phonesRoutes = require("./routes/phone.routes.js");
 app.use("/api/phones", phonesRoutes);
 
 app.get("/", (req, res) => {
-  res.send("hello world!!!");
+  res.send("hello world this is lookgin good!!!");
 });
 
 // todo: helpers
